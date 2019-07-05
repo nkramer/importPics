4f0631dac55f6ea0b4a75af64c335dab395700e1
Nick Kramer <nickkramer@outlook.com>
2019-07-04 21:39:14 -0700

    better async

--- ImportPics/MainWindow.xaml.cs
+++ ImportPics/MainWindow.xaml.cs
@@ -16,6 +16,8 @@ using System.IO;
 using Path = System.IO.Path;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Threading;
+using System.Windows.Threading;
 
 namespace ImportPics
 {
@@ -26,19 +28,39 @@ namespace ImportPics
     {
         private ObservableCollection<string> list = new ObservableCollection<string>();
 
+        private static MainWindow Instance;
+
         public MainWindow()
         {
+            Instance = this;
             InitializeComponent();
             tb.Focus();
             tb.SelectAll();
             lb.ItemsSource = list;
-            //Import("test ");
         }
 
-        private void Import(string description)
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string text = tb.Text;
+            var dispatcher = this.Dispatcher;
+            new Thread(() => BackgroundImport(text, dispatcher)).Start();
+        }
+
+        private void AddTextToUi(string line)
+        {
+            list.Add(line);
+            Debug.WriteLine(line);
+        }
+
+        private static void BackgroundAddTextToUi(string line, Dispatcher d)
+        {
+            Action a = new Action(() => Instance.AddTextToUi(line));
+            d.Invoke(a);
+        }
+
+        private static void BackgroundImport(string description, Dispatcher d)
         {
             description = description.Trim();
-            //Directory.CreateDirectory(@"")
 
             var dirs = Directory.EnumerateDirectories(@"h:\dcim");
             foreach (var dir in dirs)
@@ -51,8 +73,7 @@ namespace ImportPics
                     string prefix = dateStr + " " + description;
                     string targetDir = Path.Combine(@"E:\Pictures", prefix);
                     if (!Directory.Exists(targetDir)) {
-                        list.Add("Create " + targetDir);
-                        Debug.WriteLine("Create " + targetDir);
+                        BackgroundAddTextToUi("Create " + targetDir, d);
                         Directory.CreateDirectory(targetDir);
                     }
 
@@ -61,16 +82,12 @@ namespace ImportPics
                     newName = newName.Replace("101_", prefix + " ");
                     string newPath = Path.Combine(targetDir, newName);
 
-                    list.Add(file + " -> " + newPath);
+                    BackgroundAddTextToUi(file + " -> " + newPath, d);
                     Debug.WriteLine(file + " -> " + newPath);
                     File.Copy(file, newPath);
+                    Thread.Sleep(1000);
                 }
             }
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Import(tb.Text);
         }
     }
 }
